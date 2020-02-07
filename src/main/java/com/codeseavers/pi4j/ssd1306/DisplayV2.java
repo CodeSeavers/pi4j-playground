9fd728835e2158987549102cb849f4b300cc93aa
Alexander Hansen <alex@codeseavers.com>
2020-02-07 11:02:04 +0100

    Move to pi4j_v2

---
+++ src/main/java/com/codeseavers/pi4j/ssd1306/DisplayV2.java
@@ -0,0 +1,231 @@
+package com.codeseavers.pi4j.ssd1306;
+
+import java.io.IOException;
+
+import com.pi4j.Pi4J;
+import com.pi4j.context.Context;
+import com.pi4j.io.i2c.I2C;
+import com.pi4j.io.i2c.I2CConfig;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.Raster;
+
+public class DisplayV2 {
+
+    private final Context pi4j;
+
+    private int I2C_DEVICE = 0x3C;
+
+    private int vccState = Constants.SSD1306_SWITCHCAPVCC;
+    private int compins = 0x12;
+    private byte[] buffer;
+    private int width, height, pages;
+    protected BufferedImage img;
+    protected Graphics2D graphics;
+
+    private I2CConfig config;
+
+    public DisplayV2() throws Exception {
+        this.width = 128;
+        this.height = 64;
+        this.pages = (height / 8);
+        this.buffer = new byte[width * this.pages];
+
+        this.pi4j = Pi4J.newAutoContext();
+
+        config = I2C.newConfigBuilder(pi4j).id("my-i2c-bus").name("My IC Bus").bus(1).device(I2C_DEVICE).build();
+
+        this.img = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_BINARY);
+        this.graphics = this.img.createGraphics();
+    }
+
+    private void init() {
+        try (var i2c = pi4j.i2c().create(this.config)) {
+
+            i2c.write(Constants.SSD1306_DISPLAYOFF);
+            i2c.write(Constants.SSD1306_SETDISPLAYCLOCKDIV);
+            i2c.write(0x80);
+            i2c.write(Constants.SSD1306_SETMULTIPLEX);
+            i2c.write(0x3F);
+            i2c.write(Constants.SSD1306_SETDISPLAYOFFSET);
+            i2c.write(0x0);
+            i2c.write(Constants.SSD1306_SETSTARTLINE);
+            i2c.write(Constants.SSD1306_CHARGEPUMP);
+
+            if (this.vccState == Constants.SSD1306_EXTERNALVCC)
+                i2c.write((short) 0x10);
+            else
+                i2c.write((short) 0x14);
+
+            i2c.write(Constants.SSD1306_MEMORYMODE);
+            i2c.write((short) 0x00);
+            i2c.write((short) (Constants.SSD1306_SEGREMAP | 0x1));
+            i2c.write(Constants.SSD1306_COMSCANDEC);
+            i2c.write(Constants.SSD1306_SETCOMPINS);
+            i2c.write((short) this.compins);
+            i2c.write(Constants.SSD1306_SETCONTRAST);
+
+            if (this.vccState == Constants.SSD1306_EXTERNALVCC)
+                i2c.write((short) 0x9F);
+            else
+                i2c.write((short) 0xCF);
+
+            i2c.write(Constants.SSD1306_SETPRECHARGE);
+
+            if (this.vccState == Constants.SSD1306_EXTERNALVCC)
+                i2c.write((short) 0x22);
+            else
+                i2c.write((short) 0xF1);
+
+            i2c.write(Constants.SSD1306_SETVCOMDETECT);
+            i2c.write((short) 0x40);
+            i2c.write(Constants.SSD1306_DISPLAYALLON_RESUME);
+            i2c.write(Constants.SSD1306_NORMALDISPLAY);
+        } catch (Exception e) {
+            // Handle me properly
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Begin with SWITCHCAPVCC VCC mode
+     * 
+     * @see Constants#SSD1306_SWITCHCAPVCC
+     */
+    public void begin() throws IOException {
+        this.begin(Constants.SSD1306_SWITCHCAPVCC);
+    }
+
+    /**
+     * Begin with specified VCC mode (can be SWITCHCAPVCC or EXTERNALVCC)
+     * 
+     * @param vccState VCC mode
+     * @see Constants#SSD1306_SWITCHCAPVCC
+     * @see Constants#SSD1306_EXTERNALVCC
+     */
+    public void begin(int vccState) throws IOException {
+        this.vccState = vccState;
+        // For now ignore the reset pin stuff
+        // this.reset();
+        this.init();
+        this.write(Constants.SSD1306_DISPLAYON);
+        this.clear();
+        this.display();
+    }
+
+    private void write(int command) {
+        try (var i2c = pi4j.i2c().create(this.config)) {
+            i2c.write(command);
+        } catch (Exception e) {
+            // Handle me
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private void writeRegister(int command, int register) {
+        try (var i2c = pi4j.i2c().create(this.config)) {
+            i2c.writeRegister(register, command);
+        } catch (Exception e) {
+            // Handle me
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Clears the buffer by creating a new byte array
+     */
+    public void clear() {
+        this.buffer = new byte[this.width * this.pages];
+    }
+
+    /**
+     * Sends the buffer to the display
+     */
+    public synchronized void display() {
+        this.write(Constants.SSD1306_COLUMNADDR);
+        this.write(0);
+        this.write(this.width - 1);
+        this.write(Constants.SSD1306_PAGEADDR);
+        this.write(0);
+        this.write(this.pages - 1);
+
+        this.data(this.buffer);
+    }
+
+    /**
+     * Turns on data mode and sends data array
+     * 
+     * @param data Data array
+     */
+    private void data(byte[] data) {
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < 16; j++) {
+                this.writeRegister(0x40, data[i]);
+                i++;
+            }
+            i--;
+        }
+    }
+
+    /**
+     * Clears the screen and displays the string sent in, adding new lines as needed
+     * 
+     * @param data
+     * @param line
+     */
+    public void displayString(String... data) {
+        clearImage();
+        for (int i = 0; i < data.length; i++) {
+            graphics.drawString(data[i], 0, Constants.STRING_HEIGHT * (i + 1));
+        }
+        displayImage();
+    }
+
+    public void clearImage() {
+        this.graphics.setBackground(new Color(0, 0, 0, 0));
+        this.graphics.clearRect(0, 0, img.getWidth(), img.getHeight());
+    }
+
+    /**
+     * Copies AWT image contents to buffer. Calls display()
+     * 
+     * @see SSD1306_I2C_Display#display()
+     */
+    public synchronized void displayImage() {
+        Raster r = this.img.getRaster();
+
+        for (int y = 0; y < this.height; y++) {
+            for (int x = 0; x < this.width; x++) {
+                this.setPixel(x, y, (r.getSample(x, y, 0) > 0));
+            }
+        }
+
+        this.display();
+    }
+
+    /**
+     * Sets one pixel in the current buffer
+     * 
+     * @param x     X position
+     * @param y     Y position
+     * @param white White or black pixel
+     * @return True if the pixel was successfully set
+     */
+    public boolean setPixel(int x, int y, boolean white) {
+        if (x < 0 || x > this.width || y < 0 || y > this.height) {
+            return false;
+        }
+
+        if (white) {
+            this.buffer[x + (y / 8) * this.width] |= (1 << (y & 7));
+        } else {
+            this.buffer[x + (y / 8) * this.width] &= ~(1 << (y & 7));
+        }
+
+        return true;
+    } 
+}
--- src/main/java/com/codeseavers/pi4j/ssd1306/HelloSSD1306World.java
+++ src/main/java/com/codeseavers/pi4j/ssd1306/HelloSSD1306World.java
@@ -1,27 +1,15 @@
 package com.codeseavers.pi4j.ssd1306;
 
-import java.io.IOException;
-
-import com.pi4j.io.gpio.GpioController;
-import com.pi4j.io.gpio.GpioFactory;
-import com.pi4j.io.i2c.I2CBus;
-import com.pi4j.io.i2c.I2CFactory;
-import com.pi4j.io.i2c.I2CFactory.UnsupportedBusNumberException;
-import com.pi4j.wiringpi.I2C;
-
 public class HelloSSD1306World {
 
 	static String[] scrollText = new String[] { "Hello World!", "Heute ist Freitag", "Wie schön :-)", "Ich bin zuhause",
 			"Nachher gibt's Suppe" };
 
 	public static void main(String[] args) {
-		// create gpio controller
-		final GpioController gpio = GpioFactory.getInstance();
-		I2CBus i2c;
-		Display display = null;
+		
+		DisplayV2 display = null;
 		try {
-			i2c = I2CFactory.getInstance(I2C.CHANNEL_1);
-			display = new Display(128, 64, gpio, i2c, 0x3c);
+			display = new DisplayV2();
 			display.begin();
 
 			int i = 0;
@@ -33,7 +21,7 @@ public class HelloSSD1306World {
 					i = 0;
 				Thread.sleep(400);
 			}
-		} catch (UnsupportedBusNumberException | IOException | InterruptedException e) {
+		} catch (Exception e) {
 
 		} finally {
 			display.clear();
